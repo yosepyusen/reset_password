72a26cb20a25235fedecd0368d4c46c4e8b611ad
yosepyusen <yaguirreobregon@gmail.com>
2024-09-30 17:20:56 -0500

    class 5 medio

--- app/logicamail.php
+++ app/logicamail.php
@@ -7,6 +7,7 @@
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
 
+    require '../config/setting.php';
     require '../vendor/autoload.php';
     //----------- fin php mailer ------------
 
@@ -22,8 +23,14 @@
             
             if(filter_var($correo,FILTER_VALIDATE_EMAIL)){ //si es un email valido
 
-                if (count(ConsultaUsuarioPorEmail($correo)) > 0) {
+                $usuario = ConsultaUsuarioPorEmail($correo);
+                
+                if (count($usuario) > 0) {
                     
+                    //EnviarCorreoResetPassword($usuario);
+                    //echo $usuario[0]->name; //imprime: yosep o yusen
+                    //echo print_r($usuario); //imprime:Array ( [0] => stdClass Object ( [id_usuario] => 1 [name] => yosep [password] => $2y$10$GovLA8WuLB.kC00YdLCEeef5vvtzBY.Gg6BGCuQd9TNndfe2twQz. [rol] => admin [email] => yuchinnaxe@gmail.com [request_password] => 0 [token_password] => [expired_session] => ) ) 1
+                    EnviarCorreoResetPassword($usuario[0]->email, $usuario[0]->name);
 
                 }else{
                     $_SESSION['response'] = "No existe el ususario";
@@ -36,7 +43,7 @@
             $_SESSION['response'] = "Ingrese su email";
         }
     
-        header("location:../view/reset_password.php");
+        //header("location:../view/reset_password.php");
         
     endif;
 
@@ -60,6 +67,38 @@
             echo $th->getMessage();
         }finally{
             $conect->closeDB();
+        }
+    }
+
+    // enviar correo reste pass
+    function EnviarCorreoResetPassword($emailReceptor, $nombreReceptor){
+
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+            $mail->isSMTP(); //el envio se hace por SMTP
+            $mail->Host       = HOST;                     
+            $mail->SMTPAuth   = true; //Habilitar autenticación SMTP                            
+            $mail->Username   = USERNAME;                     
+            $mail->Password   = PASSWORD;                           
+            $mail->SMTPSecure = SMTP_SECURE; // la seguridad del envio de smtp, Habilitar el cifrado TLS implícito        
+            $mail->Port       = PORT;                                    
+        
+            //Recipients
+            $mail->setFrom('soporte@gmail.com', 'Soprte'); //quien va enviar el correo electronico
+            $mail->addAddress($emailReceptor, $nombreReceptor);     //Es quien recibe el correo
+    
+            //Content
+            $mail->isHTML(true); //enviamos en formato html
+            $mail->Subject = 'Reseteo de Password'; //este es el asunto
+            $mail->Body    = 'Usted ha solicitado un reseteo de contraseña <b>Cambiar Contraseña aqui</b>'; // es el contenido del correo
+        
+            $mail->send(); // con esto se envia el email
+            echo 'Mensaje Enviado';
+        } catch (Exception $e) {
+            echo "Mensaje no se envió. Mailer Error: {$mail->ErrorInfo}";
         }
     }
 
---
+++ config/setting.php
@@ -0,0 +1,10 @@
+<?php 
+
+    define("HOST","sandbox.smtp.mailtrap.io");
+    define("PORT","2525");
+    define("USERNAME","b94ff59625e190");
+    define("PASSWORD","478cb8f116f734");
+    define("SMTP_SECURE","TLS");
+
+    //este archivo settings no se hace asi sino se crea un archivo .env (para variable de entorno y que la informacion es sensible)
+?>
